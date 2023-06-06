05e963e48628751abe9a576e9d6bbd54a29753b7
Lee Jina <Lee Jina@LAPTOP-J05LR2NI>
2023-06-07 04:27:23 +0900

    hw13

---
+++ hw11/azimuth.m
@@ -0,0 +1,17 @@
+function az = azimuth(ENU)
+% input : ENU
+% output : az
+
+az=atan2d(ENU(:,1),ENU(:,2));
+
+num_d=size(ENU,1);
+
+for i=1:num_d
+    if az(i) < 0    
+       az(i) = az(i) + 2*pi;
+    end
+    
+    az=az';
+end
+
+end
